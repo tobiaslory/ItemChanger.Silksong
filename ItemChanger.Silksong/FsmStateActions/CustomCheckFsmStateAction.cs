09c5cdf06ebb0e4e9c6cc43f56c3a18a98779540
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-03-04 17:10:13 +0000

    Replace GetIsTrue field with a property

--- ItemChanger.Silksong/FsmStateActions/CustomCheckFsmStateAction.cs
+++ ItemChanger.Silksong/FsmStateActions/CustomCheckFsmStateAction.cs
@@ -1,12 +1,18 @@
-﻿using HutongGames.PlayMaker;
-using HutongGames.PlayMaker.Actions;
+﻿using HutongGames.PlayMaker.Actions;
 using Silksong.FsmUtil;
 
 namespace ItemChanger.Silksong.FsmStateActions;
 
+/// <summary>
+/// CheckFsmStateAction which sends an event based on the value of a predicate,
+/// and stores the result of the predicate in an FSM bool variable.
+/// 
+/// The bool variable must not be null for this to function, but the events
+/// may be null (and will not be sent in that case).
+/// </summary>
 public class CustomCheckFsmStateAction : FSMUtility.CheckFsmStateAction
 {
-    public required Func<bool> GetIsTrue;
+    public required Func<bool> GetIsTrue { get; init; }
 
     public override bool IsTrue => GetIsTrue();
 
